87dae306f3dfd8df34867126f867ee8f072e41fc
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2022-04-04 16:49:55 -0300

    Primer Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+bodyParser = require('body-parser')
+
+const { spawn, exec } = require("child_process");
+const app = express()
+const port = 3030
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/', (req, res) => {
+    console.log('Got body:', req.body)
+    var igUrl = `"rtmps://live-upload.instagram.com:443/rtmp/${req.body.key}"`
+      const child = spawn('ffmpeg', ['-rtbufsize','256M', '-re', '-i', req.body.URL, '-vf', '"scale=1080:-1:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2,setsar=1"', '-c:a', 'aac', '-ar', '44100', '-b:a', '128k', '-pix_fmt', 'yuv420p', '-profile:v', 'baseline', '-bufsize', '6000k', '-vb', '400k', '-maxrate', '1500k', '-deinterlace', '-vcodec', 'libx264', '-preset', 'veryfast', '-g', '30', '-r', '30', '-f', 'flv', igUrl], {shell: true});
+
+      child.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        res.send(`stdout: ${data}`);
+      });
+        
+      child.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+      });
+        
+      child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+      });
+});
+
+app.use('/', express.static(__dirname + '/public'));
